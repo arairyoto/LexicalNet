aea0ebd7ae4d35c124035e067bb623ee750ddb73
arairyoto <arai.ryoto@gmail.com>
2017-12-05 19:50:49 +0900

    WSD updated

--- WSD.py
+++ WSD.py
@@ -2,12 +2,20 @@ import os
 import sys
 import logging
 
+import nltk
 from nltk.corpus import stopwords
 from nltk.wsd import lesk
 from nltk.corpus import wordnet as wn
+from nltk.corpus import semcor
 import re
 import codecs
 import difflib
+
+import inspect
+import json
+import pickle
+
+from LexicalNet import LexicalNet, LexicalFeature
 
 # ログ
 logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)-7s %(message)s')
@@ -17,34 +25,176 @@ class WSD:
     def __init__(self):
         # initialize freq_table
         self._freqs = {}
+        with open('signatures.pickle', 'rb') as f:
+            self.signatures = pickle.load(f)
+
         self.stops = set(stopwords.words("english"))
+
+        self.word_counter = {}
 
     def __getitem__(self, synset):
         return self._freqs[synset]
 
-    def lesk(self, sentence):
-        res = []
-        sentence = sentence.split(' ')
+    def to_wordnet_pos(self, _pos):
+        if _pos.startswith('NN'):
+            pos = 'n'
+        elif _pos.startswith('VB'):
+            pos = 'v'
+        elif _pos.startswith('JJ'):
+            pos = 'a'
+        elif _pos.startswith('RB'):
+            pos = 'r'
+        else:
+            pos = None
 
-        for w in sentence:
-            try:
-                res.append(lesk(sentence, w).name())
-            except:
-                continue
+        return pos
 
-        return res
+    def establich_signature(self):
+        signatures = {}
+        for s in semcor.tagged_sents(tag='both'):
+            words = []
+            for tree in s:
+                if tree.label().__class__.__name__=='Lemma':
+                    words.append(tree.label().name())
+            words = set(words)
+            print(words)
+            for tree in s:
+                if tree.label().__class__.__name__=='Lemma':
+                    if tree.label().synset().name() not in signatures.keys():
+                        signatures[tree.label().synset().name()] = set()
+                    signatures[tree.label().synset().name()] |= words
 
-    def lesk_process(self, sentences):
+        with open('signatures.pickle', 'wb') as f:
+            pickle.dump(signatures, f)
+
+    def signature(self, ss):
+        if ss.name() not in self.signatures.keys():
+            signature = list(ss.definition().split())
+        else:
+            signature = list(ss.definition().split())+list(self.signatures[ss.name()])
+        return set(signature)
+
+    def lesk(self, sentence, w):
+        return lesk(sentence, w)
+
+    def _lesk(self, context_sentence, ambiguous_word, pos=None, synsets=None):
+        context = set(context_sentence)
+        if synsets is None:
+            synsets = wn.synsets(ambiguous_word)
+
+        if pos:
+            synsets = [ss for ss in synsets if str(ss.pos()) == pos]
+
+        if not synsets:
+            return None
+
+        _, sense = max((len(context.intersection(self.signature(ss))), ss) for ss in synsets)
+
+        return sense
+
+    def count_words(self, sentences):
+        logger.info('COUNTING...')
+        logger.info('TOTAL SENTENCES: %i', len(sentences))
+        self.total_count = 0
+        for idx, sentence in enumerate(sentences):
+            if idx%1000==0:
+                logger.info('PROGRESS: %i SENTENCES DONE', idx)
+            sentence = re.split(r' +', sentence.strip())
+            sentence = [w for w in sentence if not w in self.stops]
+            for w in sentence:
+                word = wn.morphy(w)
+                if word not in self.word_counter.keys():
+                    self.word_counter[word] = 0
+                self.word_counter[word] += 1
+                self.total_count += 1
+        logger.info('DONE')
+
+    def calc(self, context, signature, ln, lf):
+        res = 0
+        k = 100 # constant
+
+        for c in context:
+            c_o = ln.to_WSLObj(c)
+            for s in signature:
+                s_o = ln.to_WSLObj(s)
+                try:
+                    res += np.log(1+self.word_conter[c]*k/self.total_count)*np.log(1+self.word_conter[s]*k/self.total_count)*lf.relatedness(c_o, s_o)
+                except:
+                    continue
+
+        return res/(len(context)*len(signature))
+
+
+    def prototype(self, context_sentence, ambiguous_word, ln, lf, pos=None, synsets=None):
+        context = set(context_sentence)
+        if synsets is None:
+            synsets = wn.synsets(ambiguous_word)
+
+        if pos:
+            synsets = [ss for ss in synsets if str(ss.pos()) == pos]
+
+        if not synsets:
+            return None
+
+        _, sense = max(((self.calc(context, set(self.signature(ss)), ln, lf)), ss) for ss in synsets)
+
+        return sense
+
+    def prototype_process(self, sentences, update=True):
+        # Count words
+        self.count_words(sentences)
+
+        ln = LexicalNet()
+        lf = LexicalFeature()
+
+        logger.info('PROCESSING...')
+        logger.info('TOTAL SENTENCES: %i', len(sentences))
+
+        for idx, sentence in enumerate(sentences):
+            if idx%1==0:
+                logger.info('PROGRESS: %i SENTENCES DONE', idx)
+            sentence = re.split(r' +', sentence.strip())
+            sentence = [w for w in sentence if not w in self.stops]
+            tagged_sentence = nltk.pos_tag(sentence)
+
+            for wp in tagged_sentence:
+                try:
+                    w = wp[0] # word
+                    p = self.to_wordnet_pos(wp[1]) # pos
+                    if update:
+                        synset = self.prototype(sentence, w, ln, lf, pos=p).name()
+                    else:
+                        synset = self.prototype(sentence, w, ln, lf, pos=p).name()
+                    word = wn.morphy(w)
+                    if word not in wn.synset(synset).lemma_names():
+                        _, word = max((difflib.SequenceMatcher(None, word, l).ratio(), l) for l in wn.synset(synset).lemma_names())
+
+                    lemma = synset+':'+word
+                    if lemma not in self._freqs.keys():
+                        self._freqs[lemma] = 0
+                    self._freqs[lemma] += 1
+                except:
+                    continue
+        logger.info('DONE')
+
+    def lesk_process(self, sentences, update=True):
         logger.info('PROCESSING...')
         logger.info('TOTAL SENTENCES: %i', len(sentences))
         for idx, sentence in enumerate(sentences):
             if idx%1000==0:
                 logger.info('PROGRESS: %i SENTENCES DONE', idx)
-            sentence = re.split(r' +', sentence)
+            sentence = re.split(r' +', sentence.strip())
             sentence = [w for w in sentence if not w in self.stops]
-            for w in sentence:
+            tagged_sentence = nltk.pos_tag(sentence)
+
+            for wp in tagged_sentence:
                 try:
-                    synset = lesk(sentence, w).name()
+                    w = wp[0] # word
+                    p = self.to_wordnet_pos(wp[1]) # pos
+                    if update:
+                        synset = self._lesk(sentence, w, pos=p).name()
+                    else:
+                        synset = self.lesk(sentence, w, pos=p).name()
                     word = wn.morphy(w)
                     if word not in wn.synset(synset).lemma_names():
                         _, word = max((difflib.SequenceMatcher(None, word, l).ratio(), l) for l in wn.synset(synset).lemma_names())
@@ -106,11 +256,19 @@ class WSD:
         # output file
         self.output(output_file_name)
 
+    def _main(self, input_file_name, output_file_name):
+        # Loading input file
+        sentences = self.input(input_file_name)
+        # processing sentence
+        self.prototype_process(sentences)
+        # output file
+        self.output(output_file_name)
+
 
 if __name__=='__main__':
-
     input_file_name = 'wsd_input/choco_review_back.txt'
-    output_file_name = 'wsd_output/freq_choco.txt'
+    output_file_name = 'wsd_output/freq_choco_uprototype.txt'
 
     wsd = WSD()
-    wsd.main(input_file_name, output_file_name)
+    # wsd.establich_signature()
+    wsd._main(input_file_name, output_file_name)
